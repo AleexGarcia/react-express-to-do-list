7956b2ff31e4127dd0fb4c132927369a26a16de5
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2023-08-25 14:25:36 -0300

    rotas criadas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+import express from 'express';
+import { router } from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.listen(3001, () => console.log('Server on'));
+
